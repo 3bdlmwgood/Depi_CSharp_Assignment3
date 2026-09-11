2558037057acab15997ad6802502ba3e748ebc38
3bdlmwgood <abdullahmohamedabdelmawgood@gmail.com>
2026-09-11 18:05:17 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void PrintHeaderNumber(int Q)
+        {
+            Console.WriteLine($"=========== Question {Q} ===========");
+        }
+        
+        private static void _Run
+
+        static void Q1()
+        {
+
+
+        }
+        
+        static void Main(string[] args)
+        {
+            #region Functions
+
+
+
+            Q1();
+
+
+
+            #endregion
+        }
+    }
+}
